2c2e9cc8cae23606cae8a21821be75e002ed9746
lexrus <lexrus@gmail.com>
2016-02-07 18:57:18 +0800

    Upgrade to Swift 2.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # PhoneticContacts
 
 [![Git](https://img.shields.io/badge/GitHub-lexrus-blue.svg?style=flat)](https://github.com/lexrus)
+
 [![Twitter](https://img.shields.io/badge/twitter-@lexrus-blue.svg?style=flat)](http://twitter.com/lexrus)
-[![LinkedIn](https://img.shields.io/badge/linkedin-Lex Tang-blue.svg?style=flat)](https://cn.linkedin.com/in/lexrus/en)
-[![eMail](https://img.shields.io/badge/email-lexrus@gmail.com-blue.svg?style=flat)](mailto:lexrus@gmail.com?SUBJECT=About PhoneticContacts)
 
 为你的联系人加上拼音属性，这样即使你的 iPhone 设置成英文，也能有按拼音分段的功能。
 
 <img width="319" src="https://cloud.githubusercontent.com/assets/219689/8078530/e9482cde-0f90-11e5-8c7d-9a879110f08c.png" alt="screenshot"/>
 
-这是 Objective-C 的实现，参考了 [V2EX上网友写的 AppleScript](http://v2ex.com/t/52860)，查拼音的事可以交给 CFStringTransform ，不需要 curl ，所以速度更快更省事。
+这是 <del>Objective-C</del> Swift 2 的实现，参考了 [V2EX上网友写的 AppleScript](http://v2ex.com/t/52860)，查拼音的事可以交给 CFStringTransform ，不需要 curl ，所以速度更快更省事。在网上随便找到一些姓的特殊读音，做了替换的处理，可能不全，欢迎补充。
 
-在 Xcode 4.5+ 里打开项目，运行项目后联系人就有拼音属性了。
+在 Xcode 7.2+ 里打开项目，运行项目后联系人就有拼音属性了。
+
---
+++ main.swift
@@ -0,0 +1,131 @@
+//
+//  main.swift
+//  PhoneticContacts
+//
+//  Created by Lex on 2/7/16.
+//  Copyright © 2016 Lex Tang. All rights reserved.
+//
+
+import Foundation
+import AddressBook
+
+extension String {
+
+    func upcaseInitial() -> String {
+        var chars = characters
+        if let firstChar = chars.popFirst().map({ String($0) }) {
+            return String(firstChar).uppercaseString + String(chars)
+        }
+        return ""
+    }
+
+    func phonetic() -> String {
+        let src = NSMutableString(string: self) as CFMutableString
+        CFStringTransform(src, nil, kCFStringTransformMandarinLatin, false)
+
+        // Uncomment line below if you don't want the accents. E.g. NínHǎo to NinHao
+        CFStringTransform(src, nil, kCFStringTransformStripCombiningMarks, false)
+
+        let s = src as String
+        if s != self {
+            return s
+                .componentsSeparatedByString(" ")
+                .map { $0.upcaseInitial() }
+                .reduce("", combine: +)
+        }
+
+        return self
+    }
+
+    func phoneticLast() -> String {
+        let SpecialLastName: [String: String] = [
+            "柏": "bai",
+            "鲍": "bao",
+            "贲": "ben",
+            "秘": "bi",
+            "薄": "bo",
+            "卜": "bu",
+            "岑": "cen",
+            "晁": "chao",
+            "谌": "chen",
+            "种": "chong",
+            "褚": "chu",
+            "啜": "chuai",
+            "单": "chan",
+            "郗": "chi",
+            "邸": "di",
+            "都": "du",
+            "缪": "miao",
+            "宓": "mi",
+            "费": "fei",
+            "苻": "fu",
+            "睢": "sui",
+            "区": "ou",
+            "华": "hua",
+            "庞": "pang",
+            "查": "zha",
+            "佘": "she",
+            "仇": "qiu",
+            "靳": "jin",
+            "解": "xie",
+            "繁": "po",
+            "折": "she",
+            "员": "yun",
+            "祭": "zhai",
+            "芮": "rui",
+            "覃": "tan",
+            "牟": "mou",
+            "蕃": "pi",
+            "戚": "qi",
+            "瞿": "qu",
+            "冼": "xian",
+            "洗": "xian",
+            "郤": "xi",
+            "庹": "tuo",
+            "彤": "tong",
+            "佟": "tong",
+            "妫": "gui",
+            "句": "gou",
+            "郝": "gao",
+            "曾": "zeng",
+            "乐": "yue",
+            "蔺": "lin",
+            "隽": "juan",
+            "靳": "jin",
+            "臧": "zang",
+            "庾": "yu",
+            "詹": "zhan",
+            "禚": "zhuo",
+            "迮": "ze",
+            "尉迟": "yuchi",
+            "长孙": "zhangsun",
+            "中行": "zhonghang",
+            "万俟": "moqi",
+            "单于": "chanyu"
+        ]
+
+        if let specialLastName = SpecialLastName[self] {
+            return specialLastName
+        }
+        return self
+    }
+
+}
+
+let ab = ABAddressBook()
+ab.people().forEach {
+    guard let people = $0 as? ABPerson else {
+        return
+    }
+
+    if let lastName = people.valueForProperty(kABLastNameProperty) as? String {
+        people.setValue(lastName.phoneticLast().phonetic(), forProperty: kABLastNamePhoneticProperty)
+        print(lastName, lastName.phoneticLast().phonetic(), separator: "->", terminator: ", ")
+    }
+
+    if let firstName = people.valueForProperty(kABFirstNameProperty) as? String {
+        people.setValue(firstName.phonetic(), forProperty: kABFirstNamePhoneticProperty)
+        print(firstName, firstName.phonetic(), separator: "->", terminator: " | ")
+    }
+}
+ab.save()
